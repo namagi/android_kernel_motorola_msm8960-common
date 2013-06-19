fad3bd83dfe6fa4a834bc7e3e43f38b4029df67d
Mahesh Sivasubramanian <msivasub@codeaurora.org>
2013-06-19 15:53:51 -0500

    tick-sched: Add API to query other cores sleep time
    When cores enter a coupled cpuidle mode, the maximum time that a system can
    sleep for is the minimum of sleep times across the cores. In certain low
    power modes, the timer might not be functional and the power management code
    relies on an external hardware to bring it out of sleep.
    
    Change-Id: I54be261710380d02a45f3b9ec075f0dcfb632315
    Reviewed-on: http://gerrit.pcs.mot.com/604432
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    SLTApproved: Christopher Fries <c.fries@motorola.com>

--- include/linux/tick.h
+++ include/linux/tick.h
@@ -124,6 +124,7 @@ static inline int tick_oneshot_mode_active(void) { return 0; }
 extern void tick_nohz_stop_sched_tick(int inidle);
 extern void tick_nohz_restart_sched_tick(void);
 extern ktime_t tick_nohz_get_sleep_length(void);
+extern ktime_t tick_nohz_get_cpu_sleep_length(int cpu);
 extern u64 get_cpu_idle_time_us(int cpu, u64 *last_update_time);
 extern u64 get_cpu_iowait_time_us(int cpu, u64 *last_update_time);
 # else
