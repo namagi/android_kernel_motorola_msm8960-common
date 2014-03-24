d299d7305631a493df26354673d82308d8a4a59d
namagi <br.mladen@gmail.com>
2014-03-24 08:28:08 +0100

    sched: configure multicore/multithreading power savings
    Change-Id: I40692a11acb045e818950c68e049c4238de6e3a1

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -902,7 +902,8 @@ extern int __weak arch_sd_sibiling_asym_packing(void);
 static inline int sd_power_saving_flags(void)
 {
 	if (sched_mc_power_savings | sched_smt_power_savings)
-		return SD_BALANCE_NEWIDLE;
+		//return SD_BALANCE_NEWIDLE;
+		return SD_POWERSAVINGS_BALANCE;
 
 	return 0;
 }
